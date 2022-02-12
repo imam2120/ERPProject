69d365b48934606f206451f71bd2f5ca1f47e5b0
aminul-arif <aminul.arif69@gmail.com>
2022-02-12 13:00:44 +0600

    12_02_2022_Product_section_with_product_controller

---
+++ Git Repository/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmunaERP.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult ProductGroup()
+        {
+            return View();
+        }
+        public ActionResult ProductCategory()
+        {
+            return View();
+        }
+        public ActionResult ProductSubCategory()
+        {
+            return View();
+        }
+        public ActionResult ProductUnit()
+        {
+            return View();
+        }
+        public ActionResult ProductInformation()
+        {
+            return View();
+        }
+    }
+}
